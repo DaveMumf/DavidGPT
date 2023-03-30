cf34fb9f117afafc431bc3fa39a19d060081e561
DaveMumf <davidvillarreal.sa@gmail.com>
2023-03-29 18:46:41 -0600

    JS app and basic UI without styling

---
+++ index.js
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const configuration = new Configuration({
+  organization: "org-0QzcyeRS4kTPveTpTfSxdzDW",
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+
+  const { message } = req.body;
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {role: "user", content: `${message}`},
+    ]
+  })
+
+  res.json({
+    completion: completion.data.choices[0].message
+  })
+  
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
